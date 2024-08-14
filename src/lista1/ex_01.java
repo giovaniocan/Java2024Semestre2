e8d59031f2879915ed9e1ce4a159329e2b31d6c0
giovaniocan <giovaniocan@gmail.com>
2024-08-14 14:29:39 -0300

    made the first list

---
+++ src/lista1/ex_01.java
@@ -0,0 +1,7 @@
+package lista1;
+
+public class ex_01 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/lista1/ex_05.java
@@ -0,0 +1,9 @@
+package lista1;
+
+public class ex_05 {
+    public static void main(String[] args) {
+        for (int i = 1; i<=10 ; i++){
+            System.out.println(i);
+        }
+    }
+}
